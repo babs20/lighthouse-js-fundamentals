63762e84018155d4fb0cf56450bede15be195470
Brady <thevillemc@gmail.com>
2020-12-14 16:11:42 +0000

    Updating files

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Add root beer
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// Get the first flavour
+console.log(iceCreamFlavours[0]);
+
+// Get the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// Total Number of Flavours
+console.log(iceCreamFlavours.length)
